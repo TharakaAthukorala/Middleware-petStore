e0f6bb92092a4e4c40817f10708bc4c741a8d60f
Tharaka Athukorala <tharakaathukorala21@gmail.com>
2021-11-19 22:29:52 +0530

    pet store

---
+++ src/main/java/com/microprofile/petstore/PetstoreRestApplication.java
@@ -0,0 +1,11 @@
+package com.microprofile.petstore;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class PetstoreRestApplication extends Application {
+}
